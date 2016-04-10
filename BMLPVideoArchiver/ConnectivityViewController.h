fb1da01563008a99600fa83de11ade4a2b8a9688
Justine Gartner <GartnerJustine@gmail.com>
2016-04-10 19:55:46 -0400

    Added ConnectivityVC in storyboard

---
+++ BMLPVideoArchiver/ConnectivityViewController.h
@@ -0,0 +1,13 @@
+//
+//  ConnectivityViewController.h
+//  BMLPVideoArchiver
+//
+//  Created by Justine Kay on 4/10/16.
+//  Copyright © 2016 Justine Kay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConnectivityViewController : UIViewController
+
+@end
---
+++ BMLPVideoArchiver/ConnectivityViewController.m
@@ -0,0 +1,21 @@
+//
+//  ConnectivityViewController.m
+//  BMLPVideoArchiver
+//
+//  Created by Justine Kay on 4/10/16.
+//  Copyright © 2016 Justine Kay. All rights reserved.
+//
+
+#import "ConnectivityViewController.h"
+
+@interface ConnectivityViewController ()
+
+@end
+
+@implementation ConnectivityViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    }
+
+@end
