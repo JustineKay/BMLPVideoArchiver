cdf7917a994de28b3982fbeae044d2fb1432a05b
Justine Gartner <GartnerJustine@gmail.com>
2016-04-16 19:47:06 -0400

    Added SetPasscodeVC to alert to setting the passcode on first use

---
+++ BMLPVideoArchiver/SetPasscodeViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetPasscodeViewController.h
+//  BMLPVideoArchiver
+//
+//  Created by Justine Kay on 4/16/16.
+//  Copyright © 2016 Justine Kay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SetPasscodeViewController : UIViewController
+
+@end
---
+++ BMLPVideoArchiver/SetPasscodeViewController.m
@@ -0,0 +1,22 @@
+//
+//  SetPasscodeViewController.m
+//  BMLPVideoArchiver
+//
+//  Created by Justine Kay on 4/16/16.
+//  Copyright © 2016 Justine Kay. All rights reserved.
+//
+
+#import "SetPasscodeViewController.h"
+
+@interface SetPasscodeViewController ()
+
+@end
+
+@implementation SetPasscodeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
